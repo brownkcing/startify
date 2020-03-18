9ec2b718fa5d298ba347eae05205384115d76db1
Cyril <cyril.marcus@emirates.com>
2020-03-18 09:13:53 +0000

    edit ajax form

--- forms/contact.php
+++ forms/contact.php
@@ -1,79 +1,51 @@
 <?php
-if(isset($_POST['email'])) {
- 
-    // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "brownkcing@gmail.com";
-    $email_subject = "Your email subject line";
- 
-    function died($error) {
-        // your error code can go here
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br /><br />";
-        echo $error."<br /><br />";
-        echo "Please go back and fix these errors.<br /><br />";
-        die();
-    }
- 
- 
-    // validation expected data exists
-    if(!isset($_POST['name']) ||
-        !isset($_POST['email']) ||
-        !isset($_POST['message'])) {
-        died('We are sorry, but there appears to be a problem with the form you submitted.');       
-    }
- 
+// Proccess at only POST metheod
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // name of sender
+    $name = strip_tags(trim($_POST["name"]));
      
- 
-    $name = $_POST['name']; // required
-    $email = $_POST['email']; // required
-    $message = $_POST['message']; // required
-    $error_message = "";
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
- 
-  if(!preg_match($email_exp,$email)) {
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
-  }
- 
-    $string_exp = "/^[A-Za-z .'-]+$/";
- 
-  if(!preg_match($string_exp,$name)) {
-    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
-  }
- 
-  if(strlen($message) < 2) {
-    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
-  }
- 
-  if(strlen($error_message) > 0) {
-    died($error_message);
-  }
- 
-    $email_message = "Form details below.\n\n";
- 
+    // Email of sender
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
      
-    function clean_string($string) {
-      $bad = array("content-type","bcc:","to:","cc:","href");
-      return str_replace($bad,"",$string);
+    // Sender subject
+    $subject = strip_tags(trim($_POST["subject"]));
+     
+    // Sender Message
+    $message = trim($_POST["message"]);
+     
+    // Your email where this email will be sent
+    $your_email = "brownkcing@gmail.com";
+    //Your site name for identify
+    $your_site_name = "Example";
+     
+    // Build email subject
+    $email_subject = "[{$your_site_name}] New Message by {$name}";
+     
+    // Build Email Content
+    $email_content = "Name: {$name}\n";
+    $email_content .= "Email: {$email}\n";
+    $email_content .= "Subject: {$subject}\n";
+    $email_content .= "Message: {$message}\n";
+    
+    // Build email headers
+    $email_headers = "From: {$name} <{$email}>";
+     
+    // Send email
+    $send_email = mail($your_email, $email_subject, $email_content, $email_headers);
+    
+    // Check email sent or not
+    if($send_email){
+        // Send a 200 response code.
+        http_response_code(200);
+        echo "Thank You! Your message has been sent.";
+    } else {
+        // Send a 500 response code.
+        http_response_code(500);
+        echo "Oops! we couldn't send your message. Please try again later";
     }
- 
-     
- 
-    $email_message .= "Name: ".clean_string($first_name)."\n";
-    $email_message .= "Email: ".clean_string($email_from)."\n";
-    $email_message .= "Message: ".clean_string($comments)."\n";
- 
-// create email headers
-$headers = 'From: '.$email."\r\n".
-'Reply-To: '.$email."\r\n" .
-'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $email_subject, $email_message, $headers);  
-?>
- 
-<!-- include your own success html here -->
- 
-Thank you for contacting us. We will be in touch with you very soon.
- 
-<?php
- 
+} else {
+    // Send 403 response code
+    http_response_code(403);
+    echo "Oops! Submition problem. Please try again later";
 }
 ?>
--- vendor/php-email-form/validate.js
+++ vendor/php-email-form/validate.js
@@ -1,126 +1,34 @@
-jQuery(document).ready(function($) {
-  "use strict";
-
-  //Contact
-  $('form.php-email-form').submit(function() {
-   
-    var f = $(this).find('.form-group'),
-      ferror = false,
-      emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
-
-    f.children('input').each(function() { // run all inputs
-     
-      var i = $(this); // current input
-      var rule = i.attr('data-rule');
-
-      if (rule !== undefined) {
-        var ierror = false; // error flag for current input
-        var pos = rule.indexOf(':', 0);
-        if (pos >= 0) {
-          var exp = rule.substr(pos + 1, rule.length);
-          rule = rule.substr(0, pos);
-        } else {
-          rule = rule.substr(pos + 1, rule.length);
-        }
-
-        switch (rule) {
-          case 'required':
-            if (i.val() === '') {
-              ferror = ierror = true;
-            }
-            break;
-
-          case 'minlen':
-            if (i.val().length < parseInt(exp)) {
-              ferror = ierror = true;
-            }
-            break;
-
-          case 'email':
-            if (!emailExp.test(i.val())) {
-              ferror = ierror = true;
-            }
-            break;
-
-          case 'checked':
-            if (! i.is(':checked')) {
-              ferror = ierror = true;
-            }
-            break;
-
-          case 'regexp':
-            exp = new RegExp(exp);
-            if (!exp.test(i.val())) {
-              ferror = ierror = true;
-            }
-            break;
-        }
-        i.next('.validate').html((ierror ? (i.attr('data-msg') !== undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
-      }
+(function($) {
+    // Select the form and form message
+    var form = $('.php-email-form'),
+        form_messages = $('.sent-message');
+         
+    // Action at on submit event
+    $(form).on('submit', function(e) {
+        e.preventDefault(); // Stop browser loading
+         
+        // Get form data
+        var form_data = $(form).serialize();
+         
+        // Send Ajax Request
+        var the_request = $.ajax({
+            type: 'POST', // Request Type POST, GET, etc.
+            url: "../forms/contact.php",
+            data: form_data
+        });
+         
+        // At success
+        the_request.done(function(data){
+            form_messages.text("Success: "+data);
+             
+            // Do other things at success
+        });
+         
+        // At error
+        the_request.done(function(){
+            form_messages.text("Error: "+data);
+             
+            // Do other things at fails
+        });
     });
-    f.children('textarea').each(function() { // run all inputs
-
-      var i = $(this); // current input
-      var rule = i.attr('data-rule');
-
-      if (rule !== undefined) {
-        var ierror = false; // error flag for current input
-        var pos = rule.indexOf(':', 0);
-        if (pos >= 0) {
-          var exp = rule.substr(pos + 1, rule.length);
-          rule = rule.substr(0, pos);
-        } else {
-          rule = rule.substr(pos + 1, rule.length);
-        }
-
-        switch (rule) {
-          case 'required':
-            if (i.val() === '') {
-              ferror = ierror = true;
-            }
-            break;
-
-          case 'minlen':
-            if (i.val().length < parseInt(exp)) {
-              ferror = ierror = true;
-            }
-            break;
-        }
-        i.next('.validate').html((ierror ? (i.attr('data-msg') != undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
-      }
-    });
-    if (ferror) return false;
-    else var str = $(this).serialize();
-
-    var this_form = $(this);
-    var action = $(this).attr('action');
-
-    if( ! action ) {
-      this_form.find('.loading').slideUp();
-      this_form.find('.error-message').slideDown().html('The form action property is not set!');
-      return false;
-    }
-    
-    this_form.find('.sent-message').slideUp();
-    this_form.find('.error-message').slideUp();
-    this_form.find('.loading').slideDown();
-    
-    $.ajax({
-      type: "POST",
-      url: action,
-      data: str,
-      success: function(msg) {
-        if (msg == 'OK') {
-          this_form.find('.loading').slideUp();
-          this_form.find('.sent-message').slideDown();
-          this_form.find("input:not(input[type=submit]), textarea").val('');
-        } else {
-          this_form.find('.loading').slideUp();
-          this_form.find('.error-message').slideDown().html(msg);
-        }
-      }
-    });
-    return false;
-  });
-
-});
+})(jQuery);

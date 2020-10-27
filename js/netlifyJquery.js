c169e22468c0fd786486fc23f1a42d87a8569986
Cyril Marcus <brownkcing@gmail.com>
2020-10-27 16:35:26 +0800

    test12

--- js/netlifyJquery.js
+++ js/netlifyJquery.js
@@ -1,9 +1,10 @@
-$("#my-form").submit(function(e) {
+$("contactform").submit(function(e) {
     e.preventDefault();
   
     var $form = $(this);
     $.post($form.attr("action"), $form.serialize()).then(function() {
-      alert("Thank you!");
+        $('#registration').modal('show');
+        e.preventDefault();
     });
   });
 
